4f97d5df525fa15d846757551bddd90d892599e8
billescas <billescas@conwet.com>
2013-11-06 09:05:30 +0000

    Arreglado fallo en Gadget de WMS Viewer

--- js/conwet/Gadget.js
+++ js/conwet/Gadget.js
@@ -25,39 +25,39 @@
 use("conwet");
 
 conwet.Gadget = Class.create({
-
     initialize: function() {
-        this.centerEvent       = new conwet.events.Event('center_event');
-        this.featureInfoEvent  = new conwet.events.Event('feature_info_event');
-        this.gadgetInfoEvent   = new conwet.events.Event('map_info_event');
+        this.centerEvent = new conwet.events.Event('center_event');
+        this.featureInfoEvent = new conwet.events.Event('feature_info_event');
+        this.gadgetInfoEvent = new conwet.events.Event('map_info_event');
 
-        this.locationSlot      = new conwet.events.Slot('location_slot',      this.setMarker.bind(this));
-        this.locationInfoSlot  = new conwet.events.Slot('location_info_slot', function(location) {
+        this.locationSlot = new conwet.events.Slot('location_slot', this.setMarker.bind(this));
+        this.locationInfoSlot = new conwet.events.Slot('location_info_slot', function(location) {
             location = JSON.parse(location);
             if (typeof location == 'object') {
                 this.setInfoMarker(location);
             }
         }.bind(this));
 
-        this.wmsServiceSlot    = new conwet.events.Slot('wms_service_slot', function(service) {
-            service = JSON.parse(service);            
+        this.wmsServiceSlot = new conwet.events.Slot('wms_service_slot', function(service) {
+            service = JSON.parse(service);
             if (typeof service == 'object') {
                 if (('type' in service) && ('url' in service) && ('name' in service) && (service.url != "")) {
-                    if (service.type == "WMS"){
+                    if (service.type == "WMS") {
                         this.addWmsService(service);
-                    }else if (service.type == "WMSC"){
+                    } else if (service.type == "WMSC") {
                         this.addWmscService(service);
                     }
-                    
+
                 }
             }
         }.bind(this));
 
-        this.gadgetInfoSlot    = new conwet.events.Slot('map_info_slot', function(state) {
+        this.gadgetInfoSlot = new conwet.events.Slot('map_info_slot', function(state) {
             this.reacting_to_wiring_event = true;
             try {
                 this.updateState(state.evalJSON());
-            } catch (e) {}
+            } catch (e) {
+            }
             this.reacting_to_wiring_event = false;
         }.bind(this));
         //this.gadgetInfoSlot.addEvent(this.gadgetInfoEvent);
@@ -67,30 +67,29 @@ conwet.Gadget = Class.create({
         this.transformer = new conwet.map.ProjectionTransformer();
 
         this.cursorManager = new conwet.ui.CursorManager({
-            'onBlur'       : this._disableOtherCursors.bind(this),
-            'onMove'       : this._moveOtherCursors.bind(this)
+            'onBlur': this._disableOtherCursors.bind(this),
+            'onMove': this._moveOtherCursors.bind(this)
         });
 
         this.reacting_to_wiring_event = false;
         this.mapManager = new conwet.map.MapManager(this, {
-            onMove       : this.sendState.bind(this),
-            onBeforeDrag : function() {
+            onMove: this.sendState.bind(this),
+            onBeforeDrag: function() {
                 this.cursorManager.disableEvents();
                 this._disableOtherCursors();
             }.bind(this),
-            onAfterDrag  : function() {
+            onAfterDrag: function() {
                 this.cursorManager.enableEvents();
             }.bind(this),
-            initialZoom  : 0,
-            initialCenter : {
+            initialZoom: 0,
+            initialCenter: {
                 'lon': 0,
                 'lat': 0
             },
             cursorManager: this.cursorManager
-        });        
+        });
 
     },
-
     sendState: function(state) {
         if (!this.reacting_to_wiring_event) {
             if ("center" in state) {
@@ -99,7 +98,6 @@ conwet.Gadget = Class.create({
             this.gadgetInfoEvent.send(Object.toJSON(state));
         }
     },
-
     updateState: function(state) {
         if (typeof state == 'object') {
             if (('lonlat' in state) || ('focus' in state)) {
@@ -115,43 +113,36 @@ conwet.Gadget = Class.create({
             }
         }
     },
-
     addWmsService: function(wmsService) {
         this.mapManager.addWmsService(wmsService.name, wmsService.url);
     },
-    
     addWmscService: function(wmscService) {
         this.mapManager.addWmscService(wmscService.name, wmscService.url);
     },
-
     sendFeatureInfo: function(feature) {
         this.featureInfoEvent.send(feature);
     },
-
     sendCenter: function(lon, lat) {
         this.centerEvent.send(lon + "," + lat);
     },
-
     sendLocation: function(lon, lat) {
         MashupPlatform.wiring.pushEvent('location_event', lon + "," + lat);
     },
-
     setMarker: function(lonlat) {
         this.mapManager.setHighlightMarker(JSON.parse(lonlat));
-        
-    },
 
+    },
     setInfoMarker: function(positionInfos) {
-        if (positionInfos[0].bbox != null)
-            this.mapManager.setBox(positionInfos[0]);            
-        else        
-            this.mapManager.setEventMarker(positionInfos);
+        if (positionInfos.length) {
+            if (positionInfos[0].bbox != null)
+                this.mapManager.setBox(positionInfos[0]);
+            else
+                this.mapManager.setEventMarker(positionInfos);
+        }
     },
-
     _disableOtherCursors: function() {
         this.sendState({'focus': true});
     },
-
     _moveOtherCursors: function(x, y) {
         var lonlat = this.mapManager.getLonLatFromPixel(x, y);
         if (!lonlat)
@@ -161,18 +152,15 @@ conwet.Gadget = Class.create({
                 'lon': lonlat.lon,
                 'lat': lonlat.lat
             },
-            'focus' : false
+            'focus': false
         });
     },
-
     showMessage: function(message, permanent) {
         this.messageManager.showMessage(message, conwet.ui.MessageManager.INFO, permanent);
     },
-
     hideMessage: function() {
         this.messageManager.hideMessage();
     },
-
     showError: function(message, permanent) {
         this.messageManager.showMessage(message, conwet.ui.MessageManager.ERROR, permanent);
     }
